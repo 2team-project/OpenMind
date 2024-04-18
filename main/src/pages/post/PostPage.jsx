7de63577fdad2b84bd53cb9de9cce4b7c3af9419
wjy308 <65326956+wjy308@users.noreply.github.com>
2024-04-18 20:33:21 +0900

    시간 함수 완료

--- main/src/pages/post/PostPage.jsx
+++ main/src/pages/post/PostPage.jsx
@@ -1,10 +1,13 @@
-import FeedCard from '../../components/FeedCard'
 import ButtonShare from '../../components/ButtonShare'
+import getElapsedTime from '../../utils/getElapsedTime'
 
 function PostPage() {
   return (
     <>
       <ButtonShare />
+      <div>
+        <p>{getElapsedTime('2024-02-18T11:20:13.607757Z')}</p>
+      </div>
     </>
   )
 }

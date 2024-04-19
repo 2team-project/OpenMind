9f21d47b0378ac0d6b56f814416d695e2c9ae00b
foxholic9 <roy0915z@naver.com>
2024-04-19 15:05:14 +0900

    [feat] FeedCardAnswer에서 조건부 렌더링 기능 추가.
    FeedCardAnswer에서 여러 조건들로 상태가 변해야하는 부분 기능 구현 완료.
    
    FeedCard에서 현재 페이지가 post/{id}/Answer인지 체크한 뒤, FeedCardAnswer에 prop으로 전달했습니다. 이외 페이지에서는 그대로 사용할 수 있게 기본값을 적용시켜두었습니다.
    
    미답변, 답변 작성 중, 답변 수정 중, 답변 완료, 답변 거절 5가지 케이스의 구현 완료.
    
    현재 디자인은 일부 수정해야할 사항 남아있습니다.

--- main/src/components/FeedCardAnswer.jsx
+++ main/src/components/FeedCardAnswer.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import InputTextForm from './InputTextForm'
 import getElapsedTime from '../utils/getElapsedTime'
@@ -6,9 +6,6 @@ import getElapsedTime from '../utils/getElapsedTime'
 const StyledAnswer = styled.div`
   display: flex;
   flex-flow: row;
-`
-const StyledSubject = styled(FeedCardAnswer)`
-  display: flex;
 `
 const StyledProfile = styled.img`
   width: 2rem;
@@ -43,39 +40,81 @@ function SubjectProfile({ answer, subject }) {
   )
 }
 
-// 미답변, 답변 작성 중, 답변 수정 중, 답변 완료, 답변 거절 5가지 케이스의 구현 필요
-function FeedCardAnswer({ subject, answer, question }) {
+// isAnswerPage : 현재 페이지가 AnswerPage인지를 받습니다. AnswerPage에서만 prop으로 내리면 됩니다.
+// subject : 답변자 객체를 받습니다. 프로필 이미지, 이름 등을 표시하는데 쓰입니다.
+// question : 질문 하나를 받습니다. 질문 목록 배열에서 map 메서드로 하나만 전달해주세요.
+// editing : 현재의 FeedCard가 수정 중인지를 받습니다. AnswerPage에서만 prop으로 내리면 됩니다.
+function FeedCardAnswer({
+  isAnswerPage = false,
+  subject,
+  question,
+  editing = false,
+}) {
   const [answerContent, setAnswerContent] = useState(null)
-  const isAnswered = question.answer !== null
-  let isRejected = false
-  if (isAnswered) {
-    isRejected = answer.isRejected
-  }
+  const [isRejected, setIsRejected] = useState(false)
+  const [isAnswered, setIsAnswered] = useState(false)
+  const [answer, setAnswer] = useState(null)
 
-  if (isRejected) {
+  useEffect(() => {
+    setAnswer(question.answer ?? null)
+    setIsAnswered(answer !== null)
+    if (isAnswered) {
+      setIsRejected(answer.isRejected)
+    }
+  }, [isAnswered, answer?.isRejected])
+
+  // 답변을 한 경우
+  // 답변을 수정중일 경우 인풋 창을 보여주고 수정 중인 버튼이 보입니다.
+  if (isAnswered) {
+    if (editing) {
+      return (
+        <>
+          <SubjectProfile subject={subject} answer={answer} />
+          <InputTextForm
+            placeholder="답변을 입력해주세요"
+            buttonText="수정 완료"
+            value={answerContent}
+            onChange={(e) => setAnswerContent(e.target.value)}
+          />
+        </>
+      )
+    }
+    //답변 거절일 경우 인풋 창이나 답변 내용을 보여주지 않습니다.
+    if (isRejected) {
+      return (
+        <>
+          <SubjectProfile subject={subject} answer={answer} />
+          <RejectDiv>답변 거절</RejectDiv>
+        </>
+      )
+    }
+    //그 외의 답변 완료일 경우 답변 내용을 보여줍니다.
     return (
       <>
         <SubjectProfile subject={subject} answer={answer} />
-        <RejectDiv>답변 거절</RejectDiv>
+        {answer?.content}
       </>
     )
   } else {
-    return (
-      <StyledAnswer>
-        <SubjectProfile subject={subject} answer={answer} />
-        <div>
-          {isAnswered ? (
-            <div>{answerContent}</div>
-          ) : (
+    // 답변을 아직 하지 않은 경우
+    // 답변자의 정보와 'post/{id}/answer'페이지는 인풋 폼을 보여줍니다.
+    if (isAnswerPage) {
+      return (
+        <StyledAnswer>
+          <SubjectProfile subject={subject} answer={answer} />
+          <div>
             <InputTextForm
               placeholder="답변을 입력해주세요"
+              buttonText="답변 완료"
               value={answerContent}
               onChange={(e) => setAnswerContent(e.target.value)}
             />
-          )}
-        </div>
-      </StyledAnswer>
-    )
+          </div>
+        </StyledAnswer>
+      )
+    }
+    // 답변을 하지 않았고, answer페이지가 아니라면 아무것도 보여주지 않습니다.
+    return <></>
   }
 }
 export default FeedCardAnswer
